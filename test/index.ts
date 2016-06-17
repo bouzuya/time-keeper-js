fa2692f136fc6e229269a50d9e1bcbfadd4126e1
bouzuya <m@bouzuya.net>
2016-06-17 14:24:39 +0900

    add build & test settings

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+import * as assert from 'power-assert';
+import beater from 'beater';
+import { DateTime } from '../src/';
+
+const { test } = beater();
+
+test('DateTime', () => {
+  assert(DateTime);
+});
