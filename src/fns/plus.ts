43768f7271327758a919bf2ca5b1a27108d58141
bouzuya <m@bouzuya.net>
2016-11-20 21:26:11 +0900

    add new plus()

---
+++ src/fns/plus.ts
@@ -0,0 +1,59 @@
+import { DateTime2 } from '../date-time-2';
+import { Unit } from '../unit';
+import { get } from './get';
+import { parseISOString } from './parse-iso-string';
+import { isLeapYear } from './is-leap-year';
+
+// days in month (common year)
+const dm1 = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31];
+// days in month (leap year)
+const dm2 = [31, 29, 31, 30, 31, 30, 31, 30, 31, 30, 31];
+
+const daysInMonth = (y: number, m: number): number => {
+  return (isLeapYear(y) ? dm2 : dm1)[m - 1];
+};
+
+const dy1 = dm1.reduce((a, i) => a.concat([a[a.length - 1] + i]), [0]);
+const dy2 = dm2.reduce((a, i) => a.concat([a[a.length - 1] + i]), [0]);
+
+const dayOfYear = (y: number, m: number, d: number): number => {
+  return (isLeapYear(y) ? dy2 : dy1)[m - 1] + d;
+};
+
+const days = (y: number, m: number, d: number): number => {
+  const y1 = y - 1;
+  return y1 * 365
+    + Math.floor(y1 / 4) - Math.floor(y1 / 100) + Math.floor(y1 / 400)
+    + dayOfYear(y, m, d) - 1;
+};
+
+const addMonth = (dt: DateTime2, n: number): DateTime2 => {
+  const y1 = get(dt, 'year');
+  const m1 = get(dt, 'month');
+  const d1 = get(dt, 'date');
+  const days1 = days(y1, m1, d1);
+  const y2 = y1 + Math.floor((m1 + n - 1) / 12);
+  const m2 = Math.floor((m1 + n) % 12);
+  const d2 = Math.min(d1, daysInMonth(y2, m2));
+  const days2 = days(y2, m2, d2);
+  const offset = (days2 - days1) * 24 * 60 * 60;
+  return {
+    _t: dt._t + offset,
+    _z: dt._z
+  };
+};
+
+const plus = (dt: DateTime2, n: number, unit: Unit): DateTime2 => {
+  const date = new Date((dt._t - dt._z) * 1000);
+  switch (unit) {
+    case 'year': return addMonth(dt, 12 * n);
+    case 'month': return addMonth(dt, n);
+    case 'day': return { _t: dt._t + n * 24 * 60 * 60, _z: dt._z };
+    case 'hour': return { _t: dt._t + n * 60 * 60, _z: dt._z };
+    case 'minute': return { _t: dt._t + n * 60, _z: dt._z };
+    case 'second': return { _t: dt._t + n, _z: dt._z };
+    default: throw new Error();
+  }
+};
+
+export { plus };
---
+++ test/fns/plus.ts
@@ -0,0 +1,113 @@
+import * as assert from 'power-assert';
+import { test } from '../';
+import { parseISOString } from '../../src/fns/parse-iso-string';
+import { plus } from '../../src/fns/plus';
+import { toISOString } from '../../src/fns/to-iso-string';
+
+const category = 'fns/plus > ';
+
+test(category + '1 second', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'second');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-02T15:04:06-07:00');
+});
+
+test(category + '60 seconds', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 60, 'second');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-02T15:05:05-07:00');
+});
+
+test(category + '1 minute', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'minute');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-02T15:05:05-07:00');
+});
+
+test(category + '60 minutes', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 60, 'minute');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-02T16:04:05-07:00');
+});
+
+test(category + '1 hour', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'hour');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-02T16:04:05-07:00');
+});
+
+test(category + '24 hours', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 24, 'hour');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-03T15:04:05-07:00');
+});
+
+test(category + '1 day', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'day');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-01-03T15:04:05-07:00');
+});
+
+test(category + '31 days', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 31, 'day');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-02-02T15:04:05-07:00');
+});
+
+test(category + '1 month', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'month');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-02-02T15:04:05-07:00');
+});
+
+test(category + '12 months', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 12, 'month');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2007-01-02T15:04:05-07:00');
+});
+
+test(category + '(last day)', () => {
+  const d1 = parseISOString('2006-01-31T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'month');
+  assert(toISOString(d1) === '2006-01-31T15:04:05-07:00');
+  assert(toISOString(d2) === '2006-02-28T15:04:05-07:00');
+});
+
+test(category + '1 year', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 1, 'year');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2007-01-02T15:04:05-07:00');
+});
+
+test(category + '10 years', () => {
+  const d1 = parseISOString('2006-01-02T15:04:05-07:00');
+  const d2 = plus(d1, 10, 'year');
+  assert(toISOString(d1) === '2006-01-02T15:04:05-07:00');
+  assert(toISOString(d2) === '2016-01-02T15:04:05-07:00');
+});
+
+test(category + '(leap year)', () => {
+  const d1 = parseISOString('2001-02-28T00:00:00+09:00');
+  const d2 = plus(d1, 1, 'year');
+  assert(toISOString(d1) === '2001-02-28T00:00:00+09:00');
+  assert(toISOString(d2) === '2002-02-28T00:00:00+09:00');
+  const d3 = parseISOString('2004-02-29T00:00:00+09:00');
+  const d4 = plus(d3, 1, 'year');
+  assert(toISOString(d3) === '2004-02-29T00:00:00+09:00');
+  assert(toISOString(d4) === '2005-02-28T00:00:00+09:00');
+  const d5 = parseISOString('1999-02-28T00:00:00+09:00');
+  const d6 = plus(d5, 1, 'year');
+  assert(toISOString(d5) === '1999-02-28T00:00:00+09:00');
+  assert(toISOString(d6) === '2000-02-28T00:00:00+09:00');
+});
