10a474821d04ac74815cb0d2a2011427991abdf0
bouzuya <m@bouzuya.net>
2016-11-21 07:42:07 +0900

    update readme example

--- README.md
+++ README.md
@@ -23,28 +23,39 @@ $ npm install time-keeper
 
 ```ts
 import * as assert from 'assert';
-import { now, parseISOString, parseUNIXTime } from 'time-keeper';
+
+import { get } from 'time-keeper/get';
+import { inTimeZone } from 'time-keeper/in-time-zone';
+import { isLeapYear } from 'time-keeper/is-leap-year';
+import { now } from 'time-keeper/now';
+import { parseISOString } from 'time-keeper/parse-iso-string';
+import { parseUNIXTime } from 'time-keeper/parse-unix-time';
+import { plus } from 'time-keeper/plus';
+import { toISOString } from 'time-keeper/to-iso-string';
+import { toTimeZoneOffsetString } from 'time-keeper/to-time-zone-offset-string';
+import { toUNIXTime } from 'time-keeper/to-unix-time';
 
 const dt1 = now();
-const dt2 = parseUNIXTime(dt1.toUNIXTime());
-assert(dt1.toString() === dt2.toString());
+const dt2 = parseUNIXTime(toUNIXTime(dt1));
+assert(toUNIXTime(dt1) === toUNIXTime(dt2));
 
-const dt3 = parseISOString('2016-06-17T23:28:40+09:00');
-assert(dt3.get('year') === 2016);
-assert(dt3.get('month') === 6);
-assert(dt3.get('date') === 17);
-assert(dt3.get('hour') === 23);
-assert(dt3.get('minute') === 28);
-assert(dt3.get('second') === 40);
-assert(dt3.toTimeZoneOffsetString() === '+09:00');
+const dt3 = parseISOString('2006-06-17T23:28:40+09:00');
+assert(get(dt3, 'year') === 2006);
+assert(get(dt3, 'month') === 6);
+assert(get(dt3, 'date') === 17);
+assert(get(dt3, 'hour') === 23);
+assert(get(dt3, 'minute') === 28);
+assert(get(dt3, 'second') === 40);
+assert(toTimeZoneOffsetString(dt3) === '+09:00');
+assert(isLeapYear(dt3) === false);
 
-const dt4 = dt3.inTimeZone('+00:00');
-assert(dt3.toISOString() === '2016-06-17T23:28:40+09:00');
-assert(dt4.toISOString() === '2016-06-17T14:28:40+00:00');
+const dt4 = inTimeZone(dt3, '+02:00');
+assert(toISOString(dt3) === '2006-06-17T23:28:40+09:00');
+assert(toISOString(dt4) === '2006-06-17T16:28:40+02:00');
 
-const dt5 = dt4.plus(2, 'day');
-assert(dt4.toISOString() === '2016-06-17T14:28:40+00:00');
-assert(dt5.toISOString() === '2016-06-19T14:28:40+09:00');
+const dt5 = plus(dt4, 2, 'day');
+assert(toISOString(dt4) === '2006-06-17T16:28:40+02:00');
+assert(toISOString(dt5) === '2006-06-19T16:28:40+02:00');
 ```
 
 See: [`src/`](src/).
--- test/readme.ts
+++ test/readme.ts
@@ -1,26 +1,37 @@
 import * as assert from 'power-assert';
 import { test } from './';
-import { now, parseISOString, parseUNIXTime } from '../src/';
+
+import { get } from '../src/fns/get';
+import { inTimeZone } from '../src/fns/in-time-zone';
+import { isLeapYear } from '../src/fns/is-leap-year';
+import { now } from '../src/fns/now';
+import { parseISOString } from '../src/fns/parse-iso-string';
+import { parseUNIXTime } from '../src/fns/parse-unix-time';
+import { plus } from '../src/fns/plus';
+import { toISOString } from '../src/fns/to-iso-string';
+import { toTimeZoneOffsetString } from '../src/fns/to-time-zone-offset-string';
+import { toUNIXTime } from '../src/fns/to-unix-time';
 
 test('README example', () => {
   const dt1 = now();
-  const dt2 = parseUNIXTime(dt1.toUNIXTime());
-  assert(dt1.toString() === dt2.toString());
+  const dt2 = parseUNIXTime(toUNIXTime(dt1));
+  assert(toUNIXTime(dt1) === toUNIXTime(dt2));
 
-  const dt3 = parseISOString('2016-06-17T23:28:40+09:00');
-  assert(dt3.get('year') === 2016);
-  assert(dt3.get('month') === 6);
-  assert(dt3.get('date') === 17);
-  assert(dt3.get('hour') === 23);
-  assert(dt3.get('minute') === 28);
-  assert(dt3.get('second') === 40);
-  assert(dt3.toTimeZoneOffsetString() === '+09:00');
+  const dt3 = parseISOString('2006-06-17T23:28:40+09:00');
+  assert(get(dt3, 'year') === 2006);
+  assert(get(dt3, 'month') === 6);
+  assert(get(dt3, 'date') === 17);
+  assert(get(dt3, 'hour') === 23);
+  assert(get(dt3, 'minute') === 28);
+  assert(get(dt3, 'second') === 40);
+  assert(toTimeZoneOffsetString(dt3) === '+09:00');
+  assert(isLeapYear(dt3) === false);
 
-  const dt4 = dt3.inTimeZone('+02:00');
-  assert(dt3.toISOString() === '2016-06-17T23:28:40+09:00');
-  assert(dt4.toISOString() === '2016-06-17T16:28:40+02:00');
+  const dt4 = inTimeZone(dt3, '+02:00');
+  assert(toISOString(dt3) === '2006-06-17T23:28:40+09:00');
+  assert(toISOString(dt4) === '2006-06-17T16:28:40+02:00');
 
-  const dt5 = dt4.plus(2, 'day');
-  assert(dt4.toISOString() === '2016-06-17T16:28:40+02:00');
-  assert(dt5.toISOString() === '2016-06-19T16:28:40+02:00');
+  const dt5 = plus(dt4, 2, 'day');
+  assert(toISOString(dt4) === '2006-06-17T16:28:40+02:00');
+  assert(toISOString(dt5) === '2006-06-19T16:28:40+02:00');
 });
