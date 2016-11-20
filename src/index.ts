14145de7dda59ba5216779549cf43c1df4e7f75e
bouzuya <m@bouzuya.net>
2016-11-21 07:14:36 +0900

    remove old files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { DateTime } from './date-time';
-import { DateTimeImpl } from './date-time-impl';
-
-const now = DateTimeImpl.now;
-const parseISOString = DateTimeImpl.parseISOString;
-const parseUNIXTime = DateTimeImpl.parseUNIXTime;
-
-export { DateTime, now, parseISOString, parseUNIXTime };
